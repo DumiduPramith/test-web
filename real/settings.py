1205d913545d2cfd4dda0ff290b5c21bb54b866e
dumidu <dumidu42@gmail.com>
2020-07-25 16:37:35 +0530

    database settings add

--- real/settings.py
+++ real/settings.py
@@ -89,14 +89,20 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
+    # 'default' : {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'USER': 'dumidu@djangomysql',
+    #     'NAME': 'test',
+    #     'PASSWORD': 'Withikuliya1',
+    #     'HOST': 'djangomysql.mysql.database.azure.com',
+    #     'PORT': '3306',
+    # },
     'default' : {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'dumidu@djangomysql',
-        'NAME': 'test',
-        'PASSWORD': 'Withikuliya1',
-        'HOST': 'djangomysql.mysql.database.azure.com',
-        'PORT': '3306',
-    },
+        'ENGINE' : 'django.db.backends.mysql',
+        'OPTIONS' : {
+            'read_default_file' : '/tutorial/secure/mysql.cnf',
+        },
+    }
 }
 
 
