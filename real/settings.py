708732c98e94b5819102ef8bdeb795a1e54b35ec
DumiduPramith <dumidu42@gmail.com>
2020-09-01 20:07:41 +0530

    add B2 Storage rename database

--- real/settings.py
+++ real/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'real.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
     # 'default' : {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'USER': 'dumidu@djangomysql',
@@ -97,12 +97,12 @@ DATABASES = {
     #     'HOST': 'djangomysql.mysql.database.azure.com',
     #     'PORT': '3306',
     # },
-    'default' : {
-        'ENGINE' : 'django.db.backends.mysql',
-        'OPTIONS' : {
-            'read_default_file' : os.path.join(BASE_DIR,'mysql.cnf'),
-        },
-    },
+    # 'default' : {
+    #     'ENGINE' : 'django.db.backends.mysql',
+    #     'OPTIONS' : {
+    #         'read_default_file' : os.path.join(BASE_DIR,'mysql.cnf'),
+    #     },
+    # },
 }
 
 
